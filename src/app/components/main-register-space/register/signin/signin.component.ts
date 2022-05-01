6af17129e1db933787c7b9f1519ce5137cb18153
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-01 22:45:35 +0200

    signin component

---
+++ src/app/components/main-register-space/register/signin/signin.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  // Submit Form
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const username = form.value.username;
+    const email = form.value.email;
+    const password = form.value.password;
+    const confirmPassword = form.value.confirmPassword;
+    this.authService.signup(username, email, password, confirmPassword);
+  }
+
+
+}
