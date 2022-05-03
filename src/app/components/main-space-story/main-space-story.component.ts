00a3ff55a9ba20d68439b75de03e5628d973227a
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-03 18:46:50 +0200

    main-space-story component (all posts)

--- src/app/components/main-space-story/main-space-story.component.ts
+++ src/app/components/main-space-story/main-space-story.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { AddPostComponent } from 'src/app/layout/modal/add-post/add-post.component';
+import { ViewPostComponent } from 'src/app/layout/modal/view-post/view-post.component';
+import { PostModel } from 'src/app/models/Post/post.model';
+import { PostService } from 'src/app/services/post/post.service';
 
 @Component({
   selector: 'app-main-space-story',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-space-story.component.scss']
 })
 export class MainSpaceStoryComponent implements OnInit {
+  posts: PostModel[] = [];
+  postsSub: Subscription;
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.postService.getPosts();
+    this.postsSub = this.postService.posts$.subscribe(
+      (posts: PostModel[]) => {
+        this.posts = posts;
+      }
+    )
   }
 
+  onCreatePost() {
+    this.dialog.open(AddPostComponent, {
+      panelClass: ['col-4']
+    });
+  }
+
+  onViewPost(post: PostModel) {
+    this.postService.getPostById(post._id);
+    this.dialog.open(ViewPostComponent, {
+      panelClass: ['col-8'],
+      data: {
+        postData: post
+      }
+    });
+  }
+
+}
+
+export interface PostsModel {
+  postData: PostModel
 }
