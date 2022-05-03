84904c29ab1b80071ae26e5d85856871bd861838
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-03 18:52:14 +0200

    register & story path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-import { SigninComponent } from './components/register/signin/signin.component';
-import { SignupComponent } from './components/register/signup/signup.component';
 import { NotFoundComponent } from './layout/not-found/not-found.component';
+import { MainRegisterSpaceComponent } from './components/main-register-space/main-register-space.component';
+import { RegisterComponent } from './components/main-register-space/register/register.component';
+import { MainSpaceStoryComponent } from './components/main-space-story/main-space-story.component';
+import { AuthGuard } from './services/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'main-space-register', pathMatch: 'full' },
+
+  // Register Path
   {
-    path: 'register', component: RegisterComponent, children: [
-      { path: 'singin', component: SigninComponent },
-      { path: 'singup', component: SignupComponent },
-    ]
+    path: 'main-space-register', component: MainRegisterSpaceComponent, children: [
+      { path: 'register', component: RegisterComponent },
+    ],
   },
+  // Story Path
+  { path: 'main-space-story', component: MainSpaceStoryComponent, canActivate: [AuthGuard] },
+  // Not Found Path
   { path: '**', component: NotFoundComponent }
 ];
 
