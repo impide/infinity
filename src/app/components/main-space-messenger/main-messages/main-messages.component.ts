2e1520bac5a168bfe92643bf2d37640eff8acf01
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-12 00:16:01 +0200

    main message (chat)

---
+++ src/app/components/main-space-messenger/main-messages/main-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-messages',
+  templateUrl: './main-messages.component.html',
+  styleUrls: ['./main-messages.component.scss']
+})
+export class MainMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
