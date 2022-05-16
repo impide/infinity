e864cf849ee79512777cae728e498242095e1c48
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-16 22:22:35 +0200

    settings component

---
+++ src/app/components/main-space-setting/main-space-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-space-setting',
+  templateUrl: './main-space-setting.component.html',
+  styleUrls: ['./main-space-setting.component.scss']
+})
+export class MainSpaceSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
