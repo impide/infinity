5a0373386395977f87c8e1a8e2d1ed451ac906cd
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-19 18:10:20 +0200

    dynamic view on setting

---
+++ src/app/components/main-space-setting/view-setting/view-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-setting',
+  templateUrl: './view-setting.component.html',
+  styleUrls: ['./view-setting.component.scss']
+})
+export class ViewSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
