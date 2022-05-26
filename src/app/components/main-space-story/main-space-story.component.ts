1d4d865e30febe3fd6cecdb2bc3f2fd09a66267b
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-26 16:35:21 +0200

    main space story (feed)

--- src/app/components/main-space-story/main-space-story.component.ts
+++ src/app/components/main-space-story/main-space-story.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AddPostComponent } from 'src/app/layout/modal/add-post/add-post.component';
+import { DeletePostComponent } from 'src/app/layout/modal/delete-post/delete-post.component';
 import { ViewPostComponent } from 'src/app/layout/modal/view-post/view-post.component';
-import { PostModel } from 'src/app/models/Post/post.model';
+import { LikeModel, PostModel } from 'src/app/models/Post/post.model';
+import { AuthService } from 'src/app/services/authentification/auth.service';
 import { PostService } from 'src/app/services/post/post.service';
 
 @Component({
@@ -12,31 +14,36 @@ import { PostService } from 'src/app/services/post/post.service';
   styleUrls: ['./main-space-story.component.scss']
 })
 export class MainSpaceStoryComponent implements OnInit {
-  posts: PostModel[] = [];
-  postsSub: Subscription;
+  // Posts Observable
+  posts$: Observable<PostModel[]> = this.postService.posts$.asObservable();
 
   constructor(
     public dialog: MatDialog,
+    public authService: AuthService,
     private postService: PostService
   ) { }
 
   ngOnInit(): void {
+    // Get Posts
     this.postService.getPosts();
-    this.postsSub = this.postService.posts$.subscribe(
-      (posts: PostModel[]) => {
-        this.posts = posts;
-      }
-    )
   }
 
+  // Create a Post
   onCreatePost() {
     this.dialog.open(AddPostComponent, {
       panelClass: ['col-4']
     });
   }
 
+  // Like a Post
+  onLikePost(post: PostModel) {
+    const userId = new LikeModel();
+    userId.userId = this.authService.getCurrentUserId();
+    this.postService.addOneLike(post, userId);
+  }
+
+  // View Post in Modal
   onViewPost(post: PostModel) {
-    this.postService.getPostById(post._id);
     this.dialog.open(ViewPostComponent, {
       panelClass: ['col-8'],
       data: {
@@ -45,8 +52,22 @@ export class MainSpaceStoryComponent implements OnInit {
     });
   }
 
+  // Open Setting (Delete Post)
+  onOpenSetting(_id: string) {
+    this.dialog.open(DeletePostComponent, {
+      data: {
+        _id: _id
+      },
+      panelClass: ['col-4']
+    })
+  }
+
 }
 
 export interface PostsModel {
   postData: PostModel
+}
+
+export interface PostId {
+  _id: string;
 }
