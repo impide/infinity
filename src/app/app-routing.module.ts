ba3ca18d87ee56078bae1251e32d1b4f5be28286
Nathanael421 <nathanael.rayapin@gmail.com>
2022-05-26 16:33:22 +0200

    new routes and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { NotFoundComponent } from './layout/not-found/not-found.component';
 import { MainRegisterSpaceComponent } from './components/main-register-space/main-register-space.component';
 import { RegisterComponent } from './components/main-register-space/register/register.component';
 import { MainSpaceStoryComponent } from './components/main-space-story/main-space-story.component';
-import { AuthGuard } from './services/auth/auth.guard';
+import { AuthGuard } from './services/authentification/auth.guard';
 import { MainSpaceMessengerComponent } from './components/main-space-messenger/main-space-messenger.component';
 import { MainSpaceProfileComponent } from './components/main-space-profile/main-space-profile.component';
 import { PublicationComponent } from './layout/cards/publication/publication.component';
+import { MainSpaceSettingComponent } from './components/main-space-setting/main-space-setting.component';
+import { DashboardComponent } from './components/main-space-setting/components-setting/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main-space-register', pathMatch: 'full' },
@@ -22,6 +24,13 @@ const routes: Routes = [
   { path: 'main-space-story', component: MainSpaceStoryComponent, canActivate: [AuthGuard] },
   // Messenger Path
   { path: 'main-space-messenger', component: MainSpaceMessengerComponent, canActivate: [AuthGuard] },
+  // Setting Path
+  {
+    path: 'main-space-setting', component: MainSpaceSettingComponent, canActivate: [AuthGuard], children: [
+      { path: '', redirectTo: 'dashboard-view', pathMatch: 'full' },
+      { path: 'dashboard-view', component: DashboardComponent }
+    ]
+  },
   // Profile Path
   {
     path: 'main-space-profile/:id/:username', component: MainSpaceProfileComponent, canActivate: [AuthGuard], children: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMasonryModule } from 'ngx-masonry';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -14,6 +15,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppComponent } from './app.component';
 import { MainRegisterSpaceComponent } from './components/main-register-space/main-register-space.component';
@@ -34,6 +38,18 @@ import { UserProfileComponent } from './layout/header/user-profile/user-profile.
 import { FriendsListsComponent } from './components/main-space-messenger/friends-lists/friends-lists.component';
 import { MainMessagesComponent } from './components/main-space-messenger/main-messages/main-messages.component';
 import { PublicationComponent } from './layout/cards/publication/publication.component';
+import { MainSpaceSettingComponent } from './components/main-space-setting/main-space-setting.component';
+import { SidebarSettingComponent } from './components/main-space-setting/sidebar-setting/sidebar-setting.component';
+import { ViewSettingComponent } from './components/main-space-setting/view-setting/view-setting.component';
+import { SidebarProfileComponent } from './components/main-space-setting/sidebar-profile/sidebar-profile.component';
+import { DashboardComponent } from './components/main-space-setting/components-setting/dashboard/dashboard.component';
+import { AccountComponent } from './components/main-space-setting/components-setting/account/account.component';
+import { NotificationComponent } from './components/main-space-setting/components-setting/notification/notification.component';
+import { MessageComponent } from './components/main-space-setting/components-setting/message/message.component';
+import { StoriesComponent } from './components/main-space-profile/stories/stories.component';
+import { AddStoriesComponent } from './layout/modal/add-stories/add-stories.component';
+import { ViewStoriesComponent } from './layout/modal/view-stories/view-stories.component';
+import { UpdateStoriesComponent } from './layout/modal/update-stories/update-stories.component';
 
 
 @NgModule({
@@ -56,7 +72,19 @@ import { PublicationComponent } from './layout/cards/publication/publication.com
     UserProfileComponent,
     FriendsListsComponent,
     MainMessagesComponent,
-    PublicationComponent
+    PublicationComponent,
+    MainSpaceSettingComponent,
+    SidebarSettingComponent,
+    ViewSettingComponent,
+    SidebarProfileComponent,
+    DashboardComponent,
+    AccountComponent,
+    NotificationComponent,
+    MessageComponent,
+    StoriesComponent,
+    AddStoriesComponent,
+    ViewStoriesComponent,
+    UpdateStoriesComponent
   ],
   imports: [
     BrowserModule,
@@ -66,6 +94,7 @@ import { PublicationComponent } from './layout/cards/publication/publication.com
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
+    ReactiveFormsModule,
     MatCheckboxModule,
     NgxMasonryModule,
     MatIconModule,
@@ -73,7 +102,10 @@ import { PublicationComponent } from './layout/cards/publication/publication.com
     MatFormFieldModule,
     MatExpansionModule,
     MatButtonModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
