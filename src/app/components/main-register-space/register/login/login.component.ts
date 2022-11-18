3c1a7d4517b51dda0039d848c00a60685f3c366d
Nathanael421 <nathanael.rayapin@gmail.com>
2022-11-18 18:06:26 +0100

    update login password visibility

--- src/app/components/main-register-space/register/login/login.component.ts
+++ src/app/components/main-register-space/register/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { SnackBarService } from 'src/app/layout/snackbar/snackbar-service';
-import { AuthService } from 'src/app/services/authentification/auth.service';
+import { AuthService } from 'src/app/services/authentification/authAPI/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +8,22 @@ import { AuthService } from 'src/app/services/authentification/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  showPassword: boolean = false;
 
   constructor(
-    public authService: AuthService,
-    private snackbar: SnackBarService
+    public authService: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
   // Login into existing Account
-  onSubmit(form: NgForm) {
+  onSubmit(form: NgForm): void {
     this.authService.signin(form.value.email, form.value.password);
+  }
+
+  onShowPassword(): void {
+    this.showPassword = !this.showPassword;
   }
 
 }
